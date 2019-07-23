4b0fbb4f36ca6225d67fcb244f5355be9a0fbed5
aeragft <aera@gft.com>
2019-07-23 10:26:53 -0300

    Create strcture base app

---
+++ Modelo.Domain/Entities/CharacterEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Entities
+{
+    public abstract class CharacterEntity : BaseEntity
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public double Height { get; set; }
+        public double Weight { get; set; }
+        public string Hair { get; set; }
+        public string Eyes { get; set; }
+        public string Details { get; set; }
+        
+
+    }
+}
---
+++ Modelo.Domain/Entities/PlayerEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Entities
+{
+    class PlayerEntity : BaseEntity
+    {
+        public string Name_player { get; set; }
+        public string Gender { get; set; }
+        public DateTime Birthdate { get; set; }
+        public int CharacterId { get; set; }
+        public List<CharacterEntity> CharacterEntities { get; set; }
+
+    }
+}
