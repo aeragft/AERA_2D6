6c831a848d6ada3e9321ba0674568d94b5d22d05
aeragft <aera@gft.com>
2019-07-23 14:05:56 -0300

    Create Basic visual frontend app

--- Frontend/rpg-project-app/src/app/app-routing.module.ts
+++ Frontend/rpg-project-app/src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
+import { CharacterComponent } from './Pages/character/character.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'Character', component: CharacterComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
